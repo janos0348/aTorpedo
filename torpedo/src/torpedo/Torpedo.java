0be3a6d846cda7d08048ead181aee5d54b8d1cae
janos0348 <katonajanos0128@gmail.com>
2022-05-16 13:05:28 +0200

    torpedo alap (felosztva)

--- torpedo/src/torpedo/Torpedo.java
+++ torpedo/src/torpedo/Torpedo.java
@@ -1,21 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package torpedo;
 
-/**
- *
- * @author hallgato
- */
 public class Torpedo {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+       hajo hajo = new hajo(pozicioGenerator());
+        
+        loves();
+        ellenorzes();
+    }
+
+    private static int[] pozicioGenerator() {
+        int[] poz = new int[3];
+        return poz;
+    }
+
+    private static void loves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void ellenorzes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
---
+++ torpedo/src/torpedo/hajo.java
@@ -0,0 +1,12 @@
+
+package torpedo;
+
+
+public class hajo {
+    private int[] pozicio = new int[3];
+
+    public hajo(int[] pozicio) {
+        this.pozicio = pozicio;
+    }
+    
+}
