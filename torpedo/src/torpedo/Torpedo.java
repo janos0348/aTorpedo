dd79011d7df9f4fd649ada756b1d86433c233492
janos0348 <katonajanos0128@gmail.com>
2022-05-16 13:10:28 +0200

    elosztva

--- torpedo/src/torpedo/Torpedo.java
+++ torpedo/src/torpedo/Torpedo.java
@@ -1,27 +1,42 @@
 
 package torpedo;
 
+import java.util.Random;
+
 public class Torpedo {
-
-
+    static String eredmeny;
+    static int lovessekSzama;
+    static int hajoHossz = 3;
+    static Random rnd = new Random();
+    
     public static void main(String[] args) {
        hajo hajo = new hajo(pozicioGenerator());
         
-        loves();
-        ellenorzes();
+        loves(ellenorzes());
+        
     }
 
     private static int[] pozicioGenerator() {
-        int[] poz = new int[3];
+        int[] poz = new int[hajoHossz];
+        for (int i = 0; i < hajoHossz; i++) {
+            poz[i]= rnd.nextInt(6) + 1;
+            if (true) {
+                
+            }
+        }
         return poz;
     }
 
-    private static void loves() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static void loves(String talalat) {
+        while ( !(talalat == "sulyedt")) { 
+            
+            lovessekSzama++;
+        }
     }
 
-    private static void ellenorzes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static String ellenorzes() {
+        eredmeny = "talalt";
+        return eredmeny;
     }
     
 }
